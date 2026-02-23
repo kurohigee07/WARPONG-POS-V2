88e661a8dc16bd39f5eab16cdbe0474ad3b7411b
jar <kurohigee07@gmail.com>
2026-02-23 17:21:04 +0700

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware'); // IMPORT MIDDLEWARE
 const router = express.Router();
 
+// Route untuk register (tanpa middleware)
 router.post('/register', authController.register);
+
+// Route untuk login (tanpa middleware)
 router.post('/login', authController.login);
+
+// Route untuk logout (pake middleware, butuh token)
 router.post('/logout', authMiddleware, authController.logout);
 
 module.exports = router;
